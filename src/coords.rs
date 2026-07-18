2ef800e299fafafdd5b004fd08e5cd8a45bf8ded
Jeromos Kovács <iitsnotme214@proton.me>
2026-07-18 15:58:14 +0200

    refactor(coords): added some docs, minor improvements

--- src/coords.rs
+++ src/coords.rs
@@ -1,22 +1,26 @@
-/// coordinates
+/// earth coordinates
 #[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Default)]
 pub struct Coord {
     /// latitude: north-south
+    /// between -90 and 90, 0 is equator
     pub lat: f64,
     /// longitude: east-west
+    /// between -180 and 180
     pub lon: f64,
 }
 
 impl Coord {
+    /// return `None` on invalid earth coordinates
     pub fn opt_new(lat: impl Into<f64>, lon: impl Into<f64>) -> Option<Self> {
-        let lat = lat.into();
-        let lon = lon.into();
+        let (lat, lon) = (lat.into(), lon.into());
         if (-90. ..=90.).contains(&lat) && (-180. ..=180.).contains(&lon) {
             Some(Self { lat, lon })
         } else {
             None
         }
     }
+    /// # panics
+    /// invalid earth coordinate
     pub fn new(lat: impl Into<f64>, lon: impl Into<f64>) -> Self {
         Self::opt_new(lat, lon).expect("latitude must be between -90 and 90 degrees, longitude must be between -180 and 180 degrees")
     }
@@ -43,10 +47,15 @@ impl Coord {
     ///
     /// ```rust
     /// // the `coord`inate, we want the elevation for
-    /// let coord = srtm_reader::Coord::new(87.235, 10.4234423);
+    /// let coord = srtm_reader::Coord::new(87.235, 10.42344);
     /// let filename = coord.get_filename();
     /// assert_eq!(filename, "N87E010.hgt");
     /// ```
+    ///
+    /// # Note
+    ///
+    /// Due to the nature of hgt files we return "N45E016.hgt" for `Coord { lat: 45.0, lon: 16.0 }`
+    /// *although "N44E015.hgt" also contains it.* This is a so-called padding; weird but intended behaviour.
     pub fn get_filename(self) -> String {
         let lat_ch = if self.lat >= 0. { 'N' } else { 'S' };
         let lon_ch = if self.lon >= 0. { 'E' } else { 'W' };
